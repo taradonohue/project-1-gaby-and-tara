745729a7ca61b33ade905467a1b0c68a0f4618e2
Jennifer Cross <jenncross@users.noreply.github.com>
2020-02-28 10:09:18 -0500

    Initial folders

---
+++ Step 3/logger.py
@@ -0,0 +1,37 @@
+##################
+# FILL IN HEADER
+#################
+
+import microbit as mb
+import radio  # Needs to be imported separately
+
+# Change the channel if other microbits are interfering. (Default=7)
+radio.on()  # Turn on radio
+radio.config(channel=7, length=100)
+
+print('Program Started')
+mb.display.show(mb.Image.HAPPY)
+
+while not mb.button_a.is_pressed():  # wait for button A to be pressed to begin logging
+    mb.sleep(10)
+
+radio.send('start') # Send the word 'start' to start the receiver
+mb.sleep(1000)
+mb.display.show(mb.Image.HEART)  # Display Heart while logging
+
+
+# Read and send accelerometer data repeatedly until button A is pressed again
+while not mb.button_a.is_pressed():
+    ######################################################
+    # FILL In HERE
+    # Need to collect accelerometer and time measurements
+    # Need to format into a single string
+    # Send the string over the radio
+    ######################################################
+
+    radio.send(message)
+    mb.sleep(10)
+
+
+
+mb.display.show(mb.Image.SQUARE)  # Display Square when program ends
---
+++ Step 3/receiver.py
@@ -0,0 +1,35 @@
+##################
+# FILL IN HEADER
+#################
+
+import microbit as mb
+import radio  # Needs to be imported separately
+
+# Change the channel if other microbits are interfering. (Default=7)
+radio.on()  # Turn on radio
+radio.config(channel=7, length =100)
+
+print('Program Started')
+mb.display.show(mb.Image.HAPPY, delay=1000, clear=True)
+
+
+# Wait for start message before beginning printing
+incoming = ''
+while not incoming == 'start':
+    incoming = radio.receive()
+print('start')
+
+
+while True:
+    incoming = radio.receive() # Read from radio
+
+    if incoming is not None: # message was received
+        mb.display.show(mb.Image.HEART, delay=100, clear=True, wait=False)
+        
+        #############################################################
+        # FILL IN HERE
+        # Incoming is string sent from logger
+        # Need to parse it and reformat as a tuple for the MU plotter
+        #############################################################
+
+        mb.sleep(10)
