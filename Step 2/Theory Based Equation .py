dd88b5b64c6df79927ffff97916811bfebed2dd5
gackermannlogan <60230277+gackermannlogan@users.noreply.github.com>
2020-02-29 14:23:14 -0500

    Update Theory Based Equation .py

--- Step 2/Theory Based Equation .py
+++ Step 2/Theory Based Equation .py
@@ -1,3 +1,13 @@
 import numpy as np
-list1gaby = [10,12,14,16,18]
-array1gaby=np.arrray(list1)
+import math 
+list1 = [10,12,14,16,18]
+array1=np.array(list1)
+def period(array) :
+    n = len(array1)
+    for i in range(n):
+      T = (2* math.pi) * math.sqrt(array[i]/9.81)
+      array[i] = T
+      #np.append(array, T)
+   #   return array 
+    return array 
+print(period(array1))
