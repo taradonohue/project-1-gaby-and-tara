e19472f99949e632c1d155d1b7a27f8eb2b9e172
gackermannlogan <60230277+gackermannlogan@users.noreply.github.com>
2020-03-06 11:28:14 -0500

    Graphs!

--- Step 3/Data Analysis.py
+++ Step 3/Data Analysis.py
@@ -14,31 +14,33 @@ print(os.getcwd())
 fin = open("10inch-Trial.csv", "r")
 
 def find_tilt_x(acc_x, acc_y, acc_z):
-    y_denominator = np.sqrt((y * 2) + ((z * 2)))
+    y_denominator = np.sqrt((y ** 2) + (z ** 2))
     x_numerator = x
-    angle_x = np.atan2(x_numerator, y_denominator)
+    angle_x = np.arctan(x_numerator / y_denominator)
     np.degrees(angle_x)
     return np.degrees(angle_x)
 def find_tilt_y(acc_x, acc_y, acc_z):
-    x_denominator = np.sqrt((x * 2) + ((z * 2)))
+    x_denominator = np.sqrt((x ** 2) + (z ** 2))
     y_numerator = y
-    angle_y = np.atan2(y_numerator, x_denominator)
+    angle_y = np.arctan(y_numerator / x_denominator)
     np.degrees(angle_y)
     return np.degrees(angle_y)
 def find_tilt_z(acc_x,acc_y,acc_z):
-    x_and_y_denominator = np.sqrt((y * 2) + ((x * 2)))
+    x_and_y_denominator = np.sqrt((y ** 2) + (x ** 2))
     z_numerator = z
-    angle_z = np.atan2(z_numerator, x_and_y_denominator)
+    angle_z = np.arctan(z_numerator / x_and_y_denominator)
     np.degrees(angle_z)
     return np.degrees(angle_z)
 
-list = (np.genfromtxt(fin, delimiter = ","))
-x = list[0:1]
-y = list[1]
-z = list[2]
-print(x)
-#plt.plot()
-#plt.title()
-#plt.xlabel()
-#plt.ylabel()
-#plt.show()
+array = (np.genfromtxt(fin, delimiter = ","))
+x = np.array(array[:,0])
+y = np.array(array[:,1])
+z = np.array(array[:,2])
+x_axis = np.array(find_tilt_x(x,y,z))
+y_axis = np.array(find_tilt_y(x,y,z))
+z_axis = np.array(find_tilt_z(x,y,z))
+plt.plot(x,y,z)
+plt.title()
+plt.xlabel()
+plt.ylabel()
+plt.show()
