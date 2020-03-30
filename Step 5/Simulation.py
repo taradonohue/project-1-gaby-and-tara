4401967ca016813e24517f77c3c10edcaf503127
gackermannlogan <60230277+gackermannlogan@users.noreply.github.com>
2020-03-30 19:08:04 -0400

    Draft of Comments

--- Step 5/Simulation.py
+++ Step 5/Simulation.py
@@ -23,19 +23,29 @@ time = np.linspace(0, 10, 100000)
 
 #Functions
 def update_system(pos,vel,acc,time1,time2,l):
+#This function takes 6 parameters: position, velocity, acceleration, time1, time2 and length
+#This function calculates the new acceleration, velocity and position based on 
+#the previous or "old" values 
+#It returns the new acceleration, velocity and acceleration
     dt = time2-time1
     new_acceleration = (-9.81 * math.sin(pos))/l
     new_velocity = vel + (new_acceleration * dt)
     new_position = pos + (new_velocity * dt)
     return new_acceleration, new_velocity, new_position
 
-def print_system ():
+def print_system():
+#This function takes no parameters, has no returns and prints the time,position
+# velocity and acceleration
     print("Time:     ", time)
     print("Position: ", pos)
     print("Velocity: ", vel)
     print("Acceleration ", acc, "\n")
     
 def run_simulation(l, initialpos):
+#This function takes 2 parameters: length and initial position 
+#it uses the update_system function to calculated and then append the new accelerations,
+#velocities and positions
+#It returns the new positions, velocity and acceleration
     i = 1
     pos = [initialpos]
     vel = [0]
@@ -49,6 +59,8 @@ def run_simulation(l, initialpos):
     return pos, vel, acc
 
 def period(pos):
+#This function takes one parameter: the position 
+#this function calculates the period and plots it on a graph 
     filtered_pos = sig.medfilt(pos)
     peaks = sig.find_peaks(filtered_pos)[0]
     change = 0
@@ -65,6 +77,33 @@ def period(pos):
     plt.show()
 
 #Main Script
+#here the simulation is carried out for the 5 lengths 
+#print("10 inch Trial")
+plt.subplot(5,1,1)
+pos, vel, acc = run_simulation(0.254, np.pi/3)  
+plt.plot(time, pos)
+period(pos)
+
+#print("12 inch Trial")
+plt.subplot(5,1,2)
 pos, vel, acc = run_simulation(0.305, np.pi/3)  
+plt.plot(time, pos)
+period(pos)
+
+#print("14 inch Trial")
+plt.subplot(5,1,3)
+pos, vel, acc = run_simulation(0.356, np.pi/3)  
+plt.plot(time, pos)
+period(pos)
+
+#print("16 inch Trial")
+plt.subplot(5,1,4)
+pos, vel, acc = run_simulation(0.406, np.pi/3)  
+plt.plot(time, pos)
+period(pos)
+
+#print("18 inch Trial")
+plt.subplot(5,1,5)
+pos, vel, acc = run_simulation(0.457, np.pi/3)  
 plt.plot(time, pos)
 period(pos)
