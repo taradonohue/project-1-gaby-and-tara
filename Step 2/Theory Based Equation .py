8b57b8bbe44f7b00e5fda7dbf70bfa39a6c9fd62
taradonohue <60266732+taradonohue@users.noreply.github.com>
2020-03-30 20:14:33 -0400

    Update Theory Based Equation .py
    comments drafted

--- Step 2/Theory Based Equation .py
+++ Step 2/Theory Based Equation .py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 list1 = [0.254,0.305,0.356,0.406,0.457]
 array1=np.array(list1)
 def period(array) :
+#This function takes an array of lengths as a parameter. It finds the theoretical period times based on the given lengths,
+#and it has no return value.
     n = len(array1)
     for i in range(n):
       T = (2* np.pi) * np.sqrt(array[i]/9.81)
       array[i] = T
-    return array 
 print(period(array1))
 
+#This graphs the theoretical periods versus length so the relationship between them can be found.
 x = list1
 y = array1
 plt.scatter(x, y,)
